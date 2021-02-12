c2fbab1fd8296c546b85da2bd192c3661ac027f2
Konstantin Pozdnikin <kpozdnikin@gmail.com>
2021-02-12 10:07:27 +0300

    routes

--- packages/apps-routing/src/index.ts
+++ packages/apps-routing/src/index.ts
@@ -17,6 +17,9 @@ import democracy from './democracy';
 import explorer from './explorer';
 import extrinsics from './extrinsics';
 import js from './js';
+import nftWallet from './nft-wallet';
+import nftMint from './nft-mint';
+import nftMarket from './nft-market';
 import parachains from './parachains';
 import poll from './poll';
 import rpc from './rpc';
@@ -32,6 +35,9 @@ import treasury from './treasury';
 
 export default function create (t: TFunction): Routes {
   return [
+    nftWallet(t),
+    nftMint(t),
+    nftMarket(t),
     accounts(t),
     addresses(t),
     explorer(t),
