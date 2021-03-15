f5fe3cbc5e4dd38b8a7fbe46d532116df68720fd
akaryaginUSETECH <akaryagin@usetech.ru>
2021-03-16 01:10:33 +0300

    adding new docker and nginx config

---
+++ env-config.js
@@ -0,0 +1,6 @@
+window.process_env = {
+  escrowAddress: "5FdzbgdBGRM5FDALrnSPRybWhqKv4eiy6QUpWUdBt3v3omAU",
+  MatcherContractAddress: "5Cym1pvyNgzpy88bPXvrgZddH9WEaKHPpsEkET5pSfahKGmK",
+  mintedCollection: "3",
+  vaultAddress: "5FdzbgdBGRM5FDALrnSPRybWhqKv4eiy6QUpWUdBt3v3omAU",
+}
