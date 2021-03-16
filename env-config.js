8d151b242e56aa393e2bb2c3ae94e23e35f8e4c9
Konstantin Pozdnikin <kpozdnikin@gmail.com>
2021-03-16 12:08:13 +0300

    optimize registry decode

---
+++ env-config.js
@@ -0,0 +1,6 @@
+window.process_env = {
+  MatcherContractAddress: '5Cym1pvyNgzpy88bPXvrgZddH9WEaKHPpsEkET5pSfahKGmK',
+  escrowAddress: '5D73wtH5pqN99auP4b6KQRQAbketaSj4StkBJxACPBUAUdiq',
+  mintedCollection: '3',
+  vaultAddress: '5D73wtH5pqN99auP4b6KQRQAbketaSj4StkBJxACPBUAUdiq',
+};
