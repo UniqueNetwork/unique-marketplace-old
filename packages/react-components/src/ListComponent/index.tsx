81b68b01878c52429ef3920f4da2bc8223bad1ae
klepnev-ea <klepnev-ea>
2021-03-24 13:02:20 +0300

    NFTPAR-396

--- packages/react-components/src/ListComponent/index.tsx
+++ packages/react-components/src/ListComponent/index.tsx
@@ -4,6 +4,7 @@
 import './styles.scss';
 
 import React, { ReactText } from 'react';
+import { Tab } from 'semantic-ui-react';
 
 import { Table } from '@polkadot/react-components';
 
@@ -16,8 +17,20 @@ interface Props {
 function ListComponent (props: Props): React.ReactElement<Props> {
   const { children, empty, header } = props;
 
+  const panes = [
+    {
+      menuItem: 'My trades'
+    },
+    {
+      menuItem: 'All trades'
+    }
+  ];
+
   return (
     <div className='list-component'>
+      <Tab
+        panes={panes}
+      />
       <Table
         empty={empty || 'No items'}
         header={header}
