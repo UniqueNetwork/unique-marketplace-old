968c23c27c9f4b6b2038dce45c31c507a4c8b38d
Konstantin Pozdnikin <kpozdnikin@gmail.com>
2021-03-26 17:35:26 +0300

    attention message

--- packages/apps/src/Apps.tsx
+++ packages/apps/src/Apps.tsx
@@ -156,11 +156,11 @@ function Apps ({ className = '' }: Props): React.ReactElement<Props> {
                                 </div>
                               </div>
                             </header>
+                            <div className='attention-block'>
+                              FOR TESTING AND DEMONSTRATION ONLY. DO NOT USE LARGE AMOUNTS OF KSM. TRADE AT YOUR OWN RISK.
+                            </div>
                             <main className='app-main'>
                               <div className='app-container'>
-                                <div className='attention-block'>
-                                  FOR TESTING AND DEMONSTRATION ONLY. DO NOT RISK LARGE AMOUNTS OF KSM OR TRADE AT YOUR OWN RISK
-                                </div>
                                 {!web3Enabled &&
                                 <div className='error-block'>
                                   Please install and/or enable <a
