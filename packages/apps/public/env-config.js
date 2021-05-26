c870144f503241fa1e4e20935905ff78db684117
Konstantin Pozdnikin <kpozdnikin@gmail.com>
2021-05-26 15:39:26 +0300

    env-config

---
+++ packages/apps/public/env-config.js
@@ -0,0 +1,21 @@
+// Copyright 2017-2021 @polkadot/apps, UseTech authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+window.processEnv = {
+  canAddCollections: true,
+  canCreateCollection: true,
+  canCreateToken: true,
+  canEditCollection: true,
+  contractAddress: '5FgbNg55FCFT3j1KokxsHaEgp4wfnDMGazCLw3mqC359bY72',
+  escrowAddress: '5FdzbgdBGRM5FDALrnSPRybWhqKv4eiy6QUpWUdBt3v3omAU',
+  faviconPath: 'favicons/marketplace',
+  kusamaDecimals: 12,
+  maxGas: 1000000000000,
+  quoteId: 2,
+  showMarketActions: false,
+  showTrades: true,
+  singleCollectionMode: false,
+  uniqueCollectionId: '2',
+  value: 0,
+  walletMode: false
+};
