40e5b66698a4ada3918acf2e1fe215c7c282d5de
Konstantin Pozdnikin <kpozdnikin@gmail.com>
2021-07-06 12:02:18 +0300

    NFTPAR-628, NFTPAR-631

--- packages/react-components/src/NftDetails/index.tsx
+++ packages/react-components/src/NftDetails/index.tsx
@@ -40,8 +40,6 @@ function NftDetails ({ account, setShouldUpdateTokens }: NftDetailsProps): React
   const [tokenPriceForSale, setTokenPriceForSale] = useState<string>('');
   const { buyFee, cancelStep, deposited, escrowAddress, formatKsmBalance, getFee, kusamaBalance, readyToAskPrice, sendCurrentUserAction, setPrice, setReadyToAskPrice, setWithdrawAmount, tokenAsk, tokenDepositor, tokenInfo, transferStep, withdrawAmount } = useMarketplaceStages(account, collectionInfo, tokenId);
 
-  console.log('tokenId', tokenId);
-
   const uOwnIt = tokenInfo?.Owner?.toString() === account || (tokenAsk && tokenAsk.owner === account);
   const uSellIt = tokenAsk && tokenAsk.owner === account;
   const isOwnerEscrow = !!(!uOwnIt && tokenInfo && tokenInfo.Owner && tokenInfo.Owner.toString() === escrowAddress && tokenDepositor && (tokenAsk && tokenAsk.owner !== account));
