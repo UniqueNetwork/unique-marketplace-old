4d46fadcec4e658bcea6139baaf9c15a3c70c140
Konstantin Pozdnikin <kpozdnikin@gmail.com>
2021-07-06 10:46:17 +0300

    env-for-development

--- packages/apps/public/env-config.js
+++ packages/apps/public/env-config.js
@@ -2,22 +2,22 @@
 // SPDX-License-Identifier: Apache-2.0
 
 window.processEnv = {
-  canAddCollections: true,
+  canAddCollections: false,
   canCreateCollection: true,
   canCreateToken: true,
   canEditCollection: true,
   canEditToken: true,
   commission: 10,
-  contractAddress: '5FgbNg55FCFT3j1KokxsHaEgp4wfnDMGazCLw3mqC359bY72',
+  contractAddress: '5GPbxrVzvjRHUSQUS9BNUFe2Q4KVfsYZtG1CTRaqe51rNSAX',
   decimals: 6,
-  escrowAddress: '5FdzbgdBGRM5FDALrnSPRybWhqKv4eiy6QUpWUdBt3v3omAU',
+  escrowAddress: '5DXRqSKrXeSmYin1kLqDR74aqnjxShVo9DDdgvnPP3tAtxV4',
   faviconPath: 'favicons/marketplace',
   kusamaDecimals: 12,
   maxGas: 1000000000000,
   minPrice: 0.000001,
   quoteId: 2,
   showMarketActions: true,
-  uniqueCollectionIds: ['23', '25'],
+  uniqueCollectionIds: ['18', '23', '25'],
   value: 0,
   walletMode: false,
   wssUrl: 'wss://testnet2.uniquenetwork.io'
