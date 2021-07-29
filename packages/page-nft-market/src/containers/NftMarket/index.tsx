4a5605848eadb9f45577294f73a3b6eed8ef7dda
Konstantin Pozdnikin <kpozdnikin@gmail.com>
2021-07-29 11:45:17 +0300

    sorting

--- packages/page-nft-market/src/containers/NftMarket/index.tsx
+++ packages/page-nft-market/src/containers/NftMarket/index.tsx
@@ -6,13 +6,13 @@ import './styles.scss';
 import type { NftCollectionInterface } from '@polkadot/react-hooks/useCollection';
 
 // external imports
-import React, { memo, ReactElement, useCallback, useEffect, useState } from 'react';
+import React, { memo, ReactElement, useCallback, useEffect, useMemo, useState } from 'react';
 import InfiniteScroll from 'react-infinite-scroller';
 import { useHistory } from 'react-router';
 import Form from 'semantic-ui-react/dist/commonjs/collections/Form';
 import Header from 'semantic-ui-react/dist/commonjs/elements/Header';
 import Loader from 'semantic-ui-react/dist/commonjs/elements/Loader';
-import Dropdown from 'semantic-ui-react/dist/commonjs/modules/Dropdown';
+import Dropdown, { DropdownProps } from 'semantic-ui-react/dist/commonjs/modules/Dropdown';
 
 import ArrowDown from '@polkadot/app-nft-market/components/arrowDown';
 import ArrowUp from '@polkadot/app-nft-market/components/arrowUp';
@@ -48,15 +48,14 @@ const BuyTokens = ({ account, setShouldUpdateTokens, shouldUpdateTokens }: BuyTo
   const history = useHistory();
 
   const { getOffers, offers, offersCount, offersLoading, presetCollections } = useCollections();
-  // const { getOffers, offers, offersCount, presetCollections } = useCollections();
   const [searchString, setSearchString] = useState<string>('');
+  const [sortValue, setSortValue] = useState<string>();
   const [uniqueCollectionIds, setUniqueCollectionIds] = useState(envConfig.uniqueCollectionIds);
 
   const [collections, setCollections] = useState<NftCollectionInterface[]>([]);
   const [filters, setFilters] = useState<Filters>({ collectionIds: [...uniqueCollectionIds], traitsCount: [] });
 
   const hasMore = !!(offers && offersCount) && Object.keys(offers).length < offersCount;
-  // const [filteredCollection, setFilteredCollection] = useState<any>([]);
   const openDetailedInformationModal = useCallback((collectionId: string, tokenId: string) => {
     history.push(`/market/token-details?collectionId=${collectionId}&tokenId=${tokenId}`);
   }, [history]);
@@ -89,14 +88,40 @@ const BuyTokens = ({ account, setShouldUpdateTokens, shouldUpdateTokens }: BuyTo
     );
   }, []);
 
-  const sortOptions = [
-    { content: (optionNode(false, 'asc', 'Price')), key: 'PriceUp', text: 'Price', value: 'priceUp' },
-    { content: (optionNode(true, 'desc', 'Price')), key: 'PriceDown', text: 'Price', value: 'priceDown' },
-    { content: (optionNode(false, 'asc', 'Token ID')), key: 'TokenIDUp', text: 'Token ID', value: 'tokenIdUp' },
-    { content: (optionNode(false, 'desc', 'Token ID')), key: 'TokenIDDown', text: 'Token ID', value: 'tokenIdDown' },
-    { content: (optionNode(false, 'asc', 'Listing date')), key: 'ListingDateUp', text: 'Listing date', value: 'listingDateUp' },
-    { content: (optionNode(false, 'desc', 'Listing date')), key: 'ListingDateDown', text: 'Listing date', value: 'listingDateDown' }
-  ];
+  const sortOptions = useMemo(() => ([
+    { content: (optionNode(false, 'asc', 'Price')), key: 'PriceUp', text: 'Price', value: 'price-asc' },
+    { content: (optionNode(false, 'desc', 'Price')), key: 'PriceDown', text: 'Price', value: 'price-desc' },
+    { content: (optionNode(false, 'asc', 'Token ID')), key: 'TokenIDUp', text: 'Token ID', value: 'tokenId-asc' },
+    { content: (optionNode(false, 'desc', 'Token ID')), key: 'TokenIDDown', text: 'Token ID', value: 'tokenId-desc' },
+    { content: (optionNode(false, 'asc', 'Listing date')), key: 'ListingDateUp', text: 'Listing date', value: 'creationDate-asc' },
+    { content: (optionNode(false, 'desc', 'Listing date')), key: 'ListingDateDown', text: 'Listing date', value: 'creationDate-desc' }
+  ]), [optionNode]);
+
+  const setSort = useCallback((event: React.SyntheticEvent<HTMLElement, Event>, data: DropdownProps) => {
+    const { value } = data;
+
+    setSortValue(value?.toString());
+
+    if (value) {
+      setFilters({ ...filters, sort: `${value.toString().split('-')[1]}(${value.toString().split('-')[0]})` });
+    }
+  }, [filters]);
+
+  const currentValue = useMemo(() => {
+    if (sortValue) {
+      const currentOption = sortOptions.find((opt: { value: string }) => opt.value === sortValue);
+
+      if (currentOption) {
+        return optionNode(false, sortValue.split('-')[1], currentOption.text);
+      }
+    }
+
+    return optionNode(false, 'none', 'Sort by');
+  }, [optionNode, sortOptions, sortValue]);
+
+  const clearAllFilters = useCallback(() => {
+    setFilters({ collectionIds: envConfig.uniqueCollectionIds, traitsCount: [] });
+  }, []);
 
   useEffect(() => {
     if (shouldUpdateTokens) {
@@ -114,6 +139,8 @@ const BuyTokens = ({ account, setShouldUpdateTokens, shouldUpdateTokens }: BuyTo
     setShouldUpdateTokens('all');
   }, [setShouldUpdateTokens]);
 
+  const showSearch = false;
+
   return (
     <div className='nft-market'>
       <Header as='h1'>Market</Header>
@@ -128,42 +155,55 @@ const BuyTokens = ({ account, setShouldUpdateTokens, shouldUpdateTokens }: BuyTo
         <div className='marketplace-body'>
           <div className='collection-search-form'>
             <Form>
-              <Form.Field className='search-field'>
-                <Input
-                  className='isSmall'
-                  icon={
-                    <img
-                      alt='search'
-                      className='search-icon'
-                      src={searchIcon as string}
-                    />
-                  }
-                  isDisabled={!Object.values(offers).length}
-                  onChange={setSearchString}
-                  placeholder='Find token by collection, name or attribute'
-                  value={searchString}
-                  withLabel
-                >
-                  { offersLoading && (
-                    <Loader
-                      active
-                      inline='centered'
-                    />
-                  )}
-                  { searchString?.length > 0 && (
-                    <img
-                      alt='clear'
-                      className='clear-icon'
-                      onClick={clearSearch}
-                      src={clearIcon as string}
-                    />
-                  )}
-                </Input>
+              <Form.Field className='search-results'>
+                { offersCount && (
+                  <>
+                    <span>
+                      {offersCount} items
+                    </span>
+                    <a onClick={clearAllFilters}>Clear all filters</a>
+                  </>
+                )}
               </Form.Field>
+              { showSearch && (
+                <Form.Field className='search-field'>
+                  <Input
+                    className='isSmall'
+                    icon={
+                      <img
+                        alt='search'
+                        className='search-icon'
+                        src={searchIcon as string}
+                      />
+                    }
+                    isDisabled={!Object.values(offers).length}
+                    onChange={setSearchString}
+                    placeholder='Find token by collection, name or attribute'
+                    value={searchString}
+                    withLabel
+                  >
+                    { offersLoading && (
+                      <Loader
+                        active
+                        inline='centered'
+                      />
+                    )}
+                    { searchString?.length > 0 && (
+                      <img
+                        alt='clear'
+                        className='clear-icon'
+                        onClick={clearSearch}
+                        src={clearIcon as string}
+                      />
+                    )}
+                  </Input>
+                </Form.Field>
+              )}
               <Form.Field className='sort-field'>
                 <Dropdown
+                  onChange={setSort}
                   options={sortOptions}
-                  trigger={optionNode(false, 'asc', 'Price')}
+                  trigger={currentValue}
                 />
               </Form.Field>
             </Form>
