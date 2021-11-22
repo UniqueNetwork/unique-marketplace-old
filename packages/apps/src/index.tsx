74aaa413034b2692682b291edb716531b72ac785
pasynkov <n@pasynkov.com>
2021-11-22 13:18:50 +0400

    add sentry

--- packages/apps/src/index.tsx
+++ packages/apps/src/index.tsx
@@ -6,10 +6,23 @@ import './initSettings';
 import 'semantic-ui-css/semantic.min.css';
 import '@polkadot/react-components/i18n';
 
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Root from './Root';
+
+Sentry.init({
+  dsn: "https://89e449f507d14e13b737f5232fdce9aa@o1074997.ingest.sentry.io/6075045",
+  integrations: [new Integrations.BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 const rootId = 'root';
 const rootElement = document.getElementById(rootId);
