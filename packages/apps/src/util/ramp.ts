5eba97bc22bd7fd277c975e8d366c99de28229de
Stuart Clark <stuart@webuildbots.ai>
2021-11-23 17:52:43 +0000

    ramp: checking node env before selecting url

--- packages/apps/src/util/ramp.ts
+++ packages/apps/src/util/ramp.ts
@@ -1,4 +1,5 @@
 import {RampInstantSDK} from '@ramp-network/ramp-instant-sdk';
+import {IHostConfig} from '@ramp-network/ramp-instant-sdk/dist/types/types';
 
 const ksmSymbol = 'KSM';
 
@@ -7,11 +8,17 @@ export const onRamp = async (userAddress: string) => {
     return
   }
 
-  new RampInstantSDK({
+  const config: IHostConfig = {
     hostAppName: 'Artpool',
     hostLogoUrl: 'https://acg-public.s3.eu-west-2.amazonaws.com/branding/artpool.svg',
     swapAsset: ksmSymbol,
     userAddress: userAddress,
-    url: 'https://ri-widget-staging.firebaseapp.com/'
-  }).on('*', event => console.debug(event)).show();
+  };
+
+  if (process.env.NODE_ENV === "development") {
+    console.log("Using staging environment for ramp");
+    config.url = "https://ri-widget-staging.firebaseapp.com/"
+  }
+
+  new RampInstantSDK(config).on('*', event => console.debug(event)).show();
 };
